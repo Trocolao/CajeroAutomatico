6886c5f79e667185d0ef174ab5478004834fe015
Trocolao <daw13.2023@gmail.com>
2023-03-01 16:51:27 +0100

    Carlos Muñoz cajero con funcion main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import cajero.Cajero;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Cajero cajero1=new Cajero();
+        cajero1.retirarinero(0.9F);
+        cajero1.retirarinero(0.87F);
+        cajero1.retirarinero(0.88F);
+        cajero1.retirarinero(4753.88F);
+        cajero1.retirarinero(353.64F);
+        cajero1.retirarinero(112.21F);
+        cajero1.retirarinero(67.09F);
+        cajero1.retirarinero(253.27F);
+        cajero1.retirarinero(6.33F);
     }
 }
--- src/cajero/Cajero.java
+++ src/cajero/Cajero.java
@@ -96,15 +96,5 @@ public class Cajero {
         );
     }
 
-    public static void main(String[] args) {
-        Cajero cajero1=new Cajero();
-        cajero1.retirarinero(0.9F);
-        cajero1.retirarinero(0.87F);
-        cajero1.retirarinero(0.88F);
-        cajero1.retirarinero(4753.88F);
-        cajero1.retirarinero(353.64F);
 
-
-
-    }
 }
