7ec437fed15e0e9572ddc1199dfadc1a2f3ad050
Trocolao <daw13.2023@gmail.com>
2023-03-03 13:15:52 +0100

    Carlos Muñoz cajero automático orientado a objetos versión 5 con más casos de prueba

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         Cajero cajero1=new Cajero(2,3,
                 5,12,20,50,100,250,300,0,
                 0,0,8,0,7);
+
         dinero1.getDinero();
         cajero1.getDineroCajero();
         cajero1.getDineroDevuelveCajero();
@@ -29,6 +30,36 @@ public class Main {
         cajero1.getDineroCajero();
         cajero1.getDineroDevuelveCajero();
         System.out.println(cajero1.tostr());
+
+        Cajero cajero2=new Cajero(2,1,
+                3,50,20,50,100,250,300,10,
+                43,12,8,32,7);
+
+        dinero1.getDinero();
+        cajero2.getDineroCajero();
+        cajero2.getDineroDevuelveCajero();
+        System.out.println(cajero2.tostr());
+
+        dinero2.getDinero();
+        cajero2.getDineroCajero();
+        cajero2.getDineroDevuelveCajero();
+        System.out.println(cajero2.tostr());
+
+        dinero3.getDinero();
+        cajero2.getDineroCajero();
+        cajero2.getDineroDevuelveCajero();
+        System.out.println(cajero2.tostr());
+
+        dinero4.getDinero();
+        cajero2.getDineroCajero();
+        cajero2.getDineroDevuelveCajero();
+        System.out.println(cajero2.tostr());
+
+        Dinero dinero5=new Dinero(11200.51f);
+        dinero5.getDinero();
+        cajero2.getDineroCajero();
+        cajero2.getDineroDevuelveCajero();
+        System.out.println(cajero2.tostr());
 
 
     }
