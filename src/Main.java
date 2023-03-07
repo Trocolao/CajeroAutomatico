5bd17fb852bae4adf0873908fa8c4aad9fc26775
Trocolao <daw13.2023@gmail.com>
2023-03-07 09:20:46 +0100

    Carlos Muñoz cajero automático orientado a objetos versión 6

--- src/Main.java
+++ src/Main.java
@@ -3,64 +3,36 @@ import cajero.Dinero;
 
 public class Main {
     public static void main(String[] args) {
-        Dinero dinero1=new Dinero(4500.36f);
         Cajero cajero1=new Cajero(2,3,
                 5,12,20,50,100,250,300,0,
                 0,0,8,0,7);
-
-        dinero1.getDinero();
-        cajero1.getDineroCajero();
-        cajero1.getDineroDevuelveCajero();
-        System.out.println(cajero1.tostr());
-
-        Dinero dinero2=new Dinero(5101);
-        dinero2.getDinero();
-        cajero1.getDineroCajero();
-        cajero1.getDineroDevuelveCajero();
-        System.out.println(cajero1.tostr());
-
-        Dinero dinero3=new Dinero(12.47f);
-        dinero3.getDinero();
-        cajero1.getDineroCajero();
-        cajero1.getDineroDevuelveCajero();
-        System.out.println(cajero1.tostr());
-
-        Dinero dinero4=new Dinero(1200.51f);
-        dinero4.getDinero();
-        cajero1.getDineroCajero();
-        cajero1.getDineroDevuelveCajero();
-        System.out.println(cajero1.tostr());
-
         Cajero cajero2=new Cajero(2,1,
                 3,50,20,50,100,250,300,10,
                 43,12,8,32,7);
 
-        dinero1.getDinero();
-        cajero2.getDineroCajero();
-        cajero2.getDineroDevuelveCajero();
-        System.out.println(cajero2.tostr());
+        Dinero dinero1=new Dinero(4500.36f);
+        SacarDinero(dinero1,cajero1);
+        SacarDinero(dinero1,cajero2);
 
-        dinero2.getDinero();
-        cajero2.getDineroCajero();
-        cajero2.getDineroDevuelveCajero();
-        System.out.println(cajero2.tostr());
+        Dinero dinero2=new Dinero(5101);
+        SacarDinero(dinero2,cajero1);
+        SacarDinero(dinero2,cajero2);
 
-        dinero3.getDinero();
-        cajero2.getDineroCajero();
-        cajero2.getDineroDevuelveCajero();
-        System.out.println(cajero2.tostr());
+        Dinero dinero3=new Dinero(12.47f);
+        SacarDinero(dinero3,cajero1);
+        SacarDinero(dinero3,cajero2);
 
-        dinero4.getDinero();
-        cajero2.getDineroCajero();
-        cajero2.getDineroDevuelveCajero();
-        System.out.println(cajero2.tostr());
+        Dinero dinero4=new Dinero(1200.51f);
+        SacarDinero(dinero4,cajero1);
+        SacarDinero(dinero4,cajero2);
 
         Dinero dinero5=new Dinero(11200.51f);
-        dinero5.getDinero();
-        cajero2.getDineroCajero();
-        cajero2.getDineroDevuelveCajero();
-        System.out.println(cajero2.tostr());
+        SacarDinero(dinero5,cajero1);
+        SacarDinero(dinero5,cajero2);
+    }
 
-
+    private static void SacarDinero(Dinero dinero1, Cajero cajero1) {
+        cajero1.setDineroaSacar(dinero1);
+        System.out.println(cajero1.tostr());
     }
 }
--- src/cajero/Cajero.java
+++ src/cajero/Cajero.java
@@ -1,183 +1,187 @@
 package cajero;
-
-import static cajero.Dinero.getDinero;
-
 public class Cajero{
-    static int unidades500;
-    static int unidades200;
-    static int unidades100;
-    static int unidades50;
-    static int unidades20;
-    static int unidades10;
-    static int unidades5;
-    static int unidades2;
-    static int unidades1;
-    static int unidades05;
-    static int unidades02;
-    static int unidades01;
-    static int unidades005;
-    static int unidades002;
-    static int unidades001;
+     int unidades500;
+    int unidades200;
+    int unidades100;
+    int unidades50;
+    int unidades20;
+    int unidades10;
+    int unidades5;
+    int unidades2;
+    int unidades1;
+    int unidades05;
+    int unidades02;
+    int unidades01;
+    int unidades005;
+    int unidades002;
+    int unidades001;
 
     public Cajero(int unidades500, int unidades200, int unidades100, int unidades50,
                   int unidades20, int unidades10, int unidades5,
                   int unidades2, int unidades1,int unidades05, int unidades02,
                   int unidades01, int unidades005, int unidades002, int unidades001) {
-        Cajero.unidades500 = unidades500;
-        Cajero.unidades200 = unidades200;
-        Cajero.unidades100 = unidades100;
-        Cajero.unidades50 = unidades50;
-        Cajero.unidades20 = unidades20;
-        Cajero.unidades10 = unidades10;
-        Cajero.unidades5 = unidades5;
-        Cajero.unidades2 = unidades2;
-        Cajero.unidades1 = unidades1;
-        Cajero.unidades05 = unidades05;
-        Cajero.unidades02 = unidades02;
-        Cajero.unidades01 = unidades01;
-        Cajero.unidades005 = unidades005;
-        Cajero.unidades002 = unidades002;
-        Cajero.unidades001 = unidades001;
+        this.unidades500 = unidades500;
+        this.unidades200 = unidades200;
+        this.unidades100 = unidades100;
+        this.unidades50 = unidades50;
+        this.unidades20 = unidades20;
+        this.unidades10 = unidades10;
+        this.unidades5 = unidades5;
+        this.unidades2 = unidades2;
+        this.unidades1 = unidades1;
+        this.unidades05 = unidades05;
+        this.unidades02 = unidades02;
+        this.unidades01 = unidades01;
+        this.unidades005 = unidades005;
+        this.unidades002 = unidades002;
+        this.unidades001 = unidades001;
     }
 
 
-    public static int getUnidades500() {
+    public  int getUnidades500() {
         return unidades500;
     }
 
     public void setUnidades500(int unidades500) {
-        Cajero.unidades500 = unidades500;
+        unidades500 = unidades500;
     }
 
-    public static int getUnidades200() {
+    public int getUnidades200() {
         return unidades200;
     }
 
     public void setUnidades200(int unidades200) {
-        Cajero.unidades200 = unidades200;
+        this.unidades200 = unidades200;
     }
 
-    public static int getUnidades100() {
+    public int getUnidades100() {
         return unidades100;
     }
 
     public void setUnidades100(int unidades100) {
-        Cajero.unidades100 = unidades100;
+        this.unidades100 = unidades100;
     }
 
-    public static int getUnidades50() {
+    public int getUnidades50() {
         return unidades50;
     }
 
     public void setUnidades50(int unidades50) {
-        Cajero.unidades50 = unidades50;
+        this.unidades50 = unidades50;
     }
 
-    public static int getUnidades20() {
+    public int getUnidades20() {
         return unidades20;
     }
 
     public void setUnidades20(int unidades20) {
-        Cajero.unidades20 = unidades20;
+        this.unidades20 = unidades20;
     }
 
-    public static int getUnidades10() {
+    public int getUnidades10() {
         return unidades10;
     }
 
     public void setUnidades10(int unidades10) {
-        Cajero.unidades10 = unidades10;
+        this.unidades10 = unidades10;
     }
 
-    public static int getUnidades5() {
+    public int getUnidades5() {
         return unidades5;
     }
 
     public void setUnidades5(int unidades5) {
-        Cajero.unidades5 = unidades5;
+        this.unidades5 = unidades5;
     }
 
-    public static int getUnidades2() {
+    public int getUnidades2() {
         return unidades2;
     }
 
     public void setUnidades2(int unidades2) {
-        Cajero.unidades2 = unidades2;
+        this.unidades2 = unidades2;
     }
 
-    public static int getUnidades1() {
+    public int getUnidades1() {
         return unidades1;
     }
 
     public void setUnidades1(int unidades1) {
-        Cajero.unidades1 = unidades1;
+        this.unidades1 = unidades1;
     }
 
-    public static int getUnidades05() {
+    public int getUnidades05() {
         return unidades05;
     }
 
-    public static void setUnidades05(int unidades05) {
-        Cajero.unidades05 = unidades05;
+    public void setUnidades05(int unidades05) {
+        this.unidades05 = unidades05;
     }
 
-    public static int getUnidades02() {
+    public int getUnidades02() {
         return unidades02;
     }
 
-    public static void setUnidades02(int unidades02) {
-        Cajero.unidades02 = unidades02;
+    public void setUnidades02(int unidades02) {
+        this.unidades02 = unidades02;
     }
 
-    public static int getUnidades01() {
+    public int getUnidades01() {
         return unidades01;
     }
 
-    public static void setUnidades01(int unidades01) {
-        Cajero.unidades01 = unidades01;
+    public void setUnidades01(int unidades01) {
+        this.unidades01 = unidades01;
     }
 
-    public static int getUnidades005() {
+    public int getUnidades005() {
         return unidades005;
     }
 
-    public static void setUnidades005(int unidades005) {
-        Cajero.unidades005 = unidades005;
+    public void setUnidades005(int unidades005) {
+        this.unidades005 = unidades005;
     }
 
-    public static int getUnidades002() {
+    public int getUnidades002() {
         return unidades002;
     }
 
-    public static void setUnidades002(int unidades002) {
-        Cajero.unidades002 = unidades002;
+    public void setUnidades002(int unidades002) {
+        this.unidades002 = unidades002;
     }
 
-    public static int getUnidades001() {
+    public int getUnidades001() {
         return unidades001;
     }
 
-    public static void setUnidades001(int unidades001) {
-        Cajero.unidades001 = unidades001;
+    public void setUnidades001(int unidades001) {
+        this.unidades001 = unidades001;
     }
 
-    public static float _dinero;
-    public static float _dinero1;
-    static Billete bill500=new Billete(500);
-    static Billete bill200=new Billete(200);
-    static Billete bill100=new Billete(100);
-    static Billete bill50=new Billete(50);
-    static Billete bill20=new Billete(20);
-    static Billete bill10=new Billete(10);
-    static Billete bill5=new Billete(5);
-    static Moneda mon2e =new Moneda(2);
-    static Moneda mon1e =new Moneda(1);
-    static Moneda mon50cen =new Moneda(0.5f);
-    static Moneda mon20cen =new Moneda(0.2f);
-    static Moneda mon10cen =new Moneda(0.1f);
-    static Moneda mon5cen =new Moneda(0.05f);
-    static Moneda mon2cen =new Moneda(0.02f);
-    static Moneda mon1cen =new Moneda(0.01f);
+    public float _dinero;
+    public float _dinero1;
+    Billete bill500=new Billete(500);
+    Billete bill200=new Billete(200);
+    Billete bill100=new Billete(100);
+    Billete bill50=new Billete(50);
+    Billete bill20=new Billete(20);
+    Billete bill10=new Billete(10);
+    Billete bill5=new Billete(5);
+    Moneda mon2e =new Moneda(2);
+    Moneda mon1e =new Moneda(1);
+    Moneda mon50cen =new Moneda(0.5f);
+    Moneda mon20cen =new Moneda(0.2f);
+    Moneda mon10cen =new Moneda(0.1f);
+    Moneda mon5cen =new Moneda(0.05f);
+    Moneda mon2cen =new Moneda(0.02f);
+    Moneda mon1cen =new Moneda(0.01f);
+    Dinero _dineroasacar;
+    public void setDineroaSacar(Dinero dinero){
+       _dineroasacar=dinero;
+       getDineroDevuelveCajero();
+       getDineroCajero();
+
+    }
 
     public void getDineroCajero() {
         _dinero=getUnidades500()*bill500.getValor()+getUnidades200()*bill200.getValor()
@@ -188,7 +192,7 @@ public class Cajero{
                 +getUnidades01()*mon10cen.getValor()+getUnidades005()*mon5cen.getValor()+
                 getUnidades002()*mon2cen.getValor()+getUnidades001()*mon1cen.getValor();
     }
-    public void getDineroDevuelveCajero(){
+    public float getDineroDevuelveCajero(){
         float d=getBilletes500()*bill500.getValor()+getBilletes200()*bill200.getValor()
                 +getBilletes100()*bill100.getValor()+getBilletes50()*bill50.getValor()+
                 getBilletes20()*bill20.getValor()+getBilletes10()*bill10.getValor()
@@ -197,9 +201,10 @@ public class Cajero{
                 +getCen10()*mon10cen.getValor()+getCen5()*mon5cen.getValor()+
                 getCen2()*mon2cen.getValor()+getCen1()*mon1cen.getValor();
         _dinero1=(float) (Math.round(d*100.0)/100.0);
+        return _dinero1;
     }
 
-    public static int getCen1() {
+    public int getCen1() {
         int cen1 = (int)(getRestoMonedas2cen()/mon1cen.getValor());
         if(cen1<=getUnidades001()){
             return cen1;
@@ -208,7 +213,7 @@ public class Cajero{
         }
     }
 
-    public static float getRestoMonedas2cen() {
+    public float getRestoMonedas2cen() {
         float restoMonedas2cen1 = (getRestomonedas5cen()% mon2cen.getValor());
         float restoMonedas2cen= (float) (Math.round(restoMonedas2cen1*100.0)/100.0);
         int cen2 = (int)(getRestomonedas5cen()/ mon2cen.getValor());
@@ -220,7 +225,7 @@ public class Cajero{
         }
     }
 
-    public static int getCen2() {
+    public int getCen2() {
         int cen2 = (int)(getRestomonedas5cen()/ mon2cen.getValor());
         if(cen2<=getUnidades002()){
             return cen2;
@@ -229,7 +234,7 @@ public class Cajero{
         }
     }
 
-    public static float getRestomonedas5cen() {
+    public float getRestomonedas5cen() {
         float restoMonedas5cen1 = (getRestoMonedas10cen()% mon5cen.getValor());
         float restoMonedas5cen= (float) (Math.round(restoMonedas5cen1*100.0)/100.0);
         int cen5 = (int)(getRestoMonedas10cen()/ mon5cen.getValor());
@@ -241,7 +246,7 @@ public class Cajero{
         }
     }
 
-    public static int getCen5() {
+    public int getCen5() {
         int cen5 = (int)(getRestoMonedas10cen()/ mon5cen.getValor());
         if(cen5<=getUnidades005()){
             return cen5;
@@ -250,7 +255,7 @@ public class Cajero{
         }
     }
 
-    public static float getRestoMonedas10cen() {
+    public float getRestoMonedas10cen() {
         float restoMonedas10cen1 = (getRestoMonedas20cen()% mon10cen.getValor());
         float restomonedas10cen= (float) (Math.round(restoMonedas10cen1*100.0)/100.0);
         int cen10 = (int)(getRestoMonedas20cen()/ mon10cen.getValor());
@@ -262,7 +267,7 @@ public class Cajero{
         }
     }
 
-    public static int getCen10() {
+    public int getCen10() {
         int cen10 = (int)(getRestoMonedas20cen()/ mon10cen.getValor());
         if(cen10<=getUnidades01()){
             return cen10;
@@ -271,7 +276,7 @@ public class Cajero{
         }
     }
 
-    public static float getRestoMonedas20cen() {
+    public float getRestoMonedas20cen() {
         float restoMonedas20cen1 = (getRestoMonedas50cen()% mon20cen.getValor());
         float restomonedas20cen= (float) (Math.round(restoMonedas20cen1*100.0)/100.0);
         int cen20 = (int)(getRestoMonedas50cen()/ mon20cen.getValor());
@@ -283,7 +288,7 @@ public class Cajero{
         }
     }
 
-    public static int getCen20() {
+    public int getCen20() {
         int cen20 = (int)(getRestoMonedas50cen()/ mon20cen.getValor());
         if(cen20<=getUnidades02()){
             return cen20;
@@ -292,7 +297,7 @@ public class Cajero{
         }
     }
 
-    public static float getRestoMonedas50cen() {
+    public float getRestoMonedas50cen() {
         float restoMonedas50cen1 = getRestomonedas1e()% mon50cen.getValor();
         float restoMoneas50cen= (float) (Math.round(restoMonedas50cen1*100.0)/100.0);
         int cen50 =(int) (getRestomonedas1e()/ mon50cen.getValor());
@@ -304,7 +309,7 @@ public class Cajero{
         }
     }
 
-    public static int getCen50() {
+    public int getCen50() {
         int cen50 =(int) (getRestomonedas1e()/ mon50cen.getValor());
         if(cen50<=getUnidades05()){
             return cen50;
@@ -312,7 +317,7 @@ public class Cajero{
             return getUnidades05();
         }
     }
-    public static float getRestomonedas1e(){
+    public float getRestomonedas1e(){
         float restoMonedas1e= getRestoMonedas2e()% mon1e.getValor();
         int monedas1= (int) getRestoMonedas2e();
         if(monedas1<=getUnidades1()){
@@ -322,7 +327,7 @@ public class Cajero{
         }
     }
 
-    public static int getMonedas1() {
+    public int getMonedas1() {
         int monedas1= (int) getRestoMonedas2e();
         if(monedas1<=getUnidades1()){
             return monedas1;
@@ -330,7 +335,7 @@ public class Cajero{
             return getUnidades1();
         }    }
 
-    public static float getRestoMonedas2e() {
+    public float getRestoMonedas2e() {
         float restoMonedas2e= getRestoBilletes5() % mon2e.getValor();
         int monedas2 = (int)(getRestoBilletes5() / mon2e.getValor());
         if(monedas2<=getUnidades2()){
@@ -340,7 +345,7 @@ public class Cajero{
         }
     }
 
-    public static int getMonedas2() {
+    public int getMonedas2() {
         int monedas2 = (int)(getRestoBilletes5() / mon2e.getValor());
         if(monedas2<=getUnidades2()){
             return monedas2;
@@ -349,7 +354,7 @@ public class Cajero{
         }
     }
 
-    public static float getRestoBilletes5() {
+    public float getRestoBilletes5() {
         float restoBilletes5= getRestobilletes10() %bill5.getValor();
         int billetes5 = (int)(getRestobilletes10() / bill5.getValor());
         if(billetes5<=getUnidades5()){
@@ -359,7 +364,7 @@ public class Cajero{
         }
     }
 
-    public static int getBilletes5() {
+    public int getBilletes5() {
         int billetes5 = (int)(getRestobilletes10() / bill5.getValor());
         if(billetes5<=getUnidades5()){
             return billetes5;
@@ -368,7 +373,7 @@ public class Cajero{
         }
     }
 
-    public static float getRestobilletes10() {
+    public float getRestobilletes10() {
         float restoBilletes10= getRestoBilletes20() %bill10.getValor();
         int billetes10 = (int)(getRestoBilletes20() / bill10.getValor());
         if(billetes10<=getUnidades10()){
@@ -378,7 +383,7 @@ public class Cajero{
         }
     }
 
-    public static int getBilletes10() {
+    public int getBilletes10() {
         int billetes10 = (int)(getRestoBilletes20() / bill10.getValor());
         if(billetes10<=getUnidades10()){
             return billetes10;
@@ -387,7 +392,7 @@ public class Cajero{
         }
     }
 
-    public static float getRestoBilletes20() {
+    public float getRestoBilletes20() {
         float restoBillete20= getRestoBilletes50() %bill20.getValor();
         int billetes20 = (int)(getRestoBilletes50() / bill20.getValor());
         if(billetes20<=getUnidades20()){
@@ -397,7 +402,7 @@ public class Cajero{
         }
     }
 
-    public static int getBilletes20() {
+    public int getBilletes20() {
         int billetes20 = (int)(getRestoBilletes50() / bill20.getValor());
         if(billetes20<=getUnidades20()){
             return billetes20;
@@ -406,7 +411,7 @@ public class Cajero{
         }
     }
 
-    public static float getRestoBilletes50() {
+    public float getRestoBilletes50() {
         float restobilletes50= getRestoBilletes100() %bill50.getValor();
         int billetes50 = (int)(getRestoBilletes100() / bill50.getValor());
         if(billetes50<=getUnidades50()){
@@ -416,7 +421,7 @@ public class Cajero{
         }
     }
 
-    public static int getBilletes50() {
+    public int getBilletes50() {
         int billetes50 = (int)(getRestoBilletes100() / bill50.getValor());
         if(billetes50<=getUnidades50()){
             return billetes50;
@@ -425,7 +430,7 @@ public class Cajero{
         }
     }
 
-    public static float getRestoBilletes100() {
+    public  float getRestoBilletes100() {
         float restoBilletes100= getRestoBilletes200() %bill100.getValor();
         int billetes100 = (int)(getRestoBilletes200() / bill100.getValor());
         if(billetes100<=getUnidades100()){
@@ -435,7 +440,7 @@ public class Cajero{
         }
     }
 
-    public static int getBilletes100( ) {
+    public int getBilletes100( ) {
         int billetes100 = (int)(getRestoBilletes200() / bill100.getValor());
         if(billetes100<=getUnidades100()){
             return billetes100;
@@ -445,7 +450,7 @@ public class Cajero{
 
     }
 
-    public static float getRestoBilletes200() {
+    public  float getRestoBilletes200() {
         float restoBilletes200= getRestoBilletes500() %bill200.getValor();
         int billetes200 = (int) (getRestoBilletes500() / bill200.getValor());
         if(billetes200<=getUnidades200()){
@@ -456,7 +461,7 @@ public class Cajero{
 
     }
 
-    public static int getBilletes200() {
+    public int getBilletes200() {
 
         int billetes200 = (int) (getRestoBilletes500() / bill200.getValor());
         if(billetes200<=getUnidades200()){
@@ -466,10 +471,10 @@ public class Cajero{
         }
     }
 
-    public static float getRestoBilletes500() {
+    public  float getRestoBilletes500() {
 
-        float restoBilletes500= getDinero() %bill500.getValor();
-        int billetes500 = (int)(getDinero() / bill500.getValor());
+        float restoBilletes500= _dineroasacar.getDinero() %bill500.getValor();
+        int billetes500 = (int)(_dineroasacar.getDinero() / bill500.getValor());
         if(billetes500<=getUnidades500()){
             return restoBilletes500;
         }else{
@@ -478,9 +483,9 @@ public class Cajero{
 
     }
 
-    public static int getBilletes500() {
+    public int getBilletes500() {
 
-        int billetes500 = (int)(getDinero() / bill500.getValor());
+        int billetes500 = (int)(_dineroasacar.getDinero() / bill500.getValor());
         if(billetes500<=getUnidades500()){
             return billetes500;
         }else{
@@ -489,13 +494,13 @@ public class Cajero{
 
     }
 
-    public static String tostr(){
-        if(getDinero()>_dinero){
+    public  String tostr(){
+        if(_dineroasacar.getDinero()>_dinero){
             return "El cajero automático no dispone de más dinero, por favor acuda al más cercano\n";
-        } else if (getDinero()!=_dinero1) {
+        } else if (_dineroasacar.getDinero()!=_dinero1) {
             return "El cajero no dispone de los cambios disponibles para retirar su dinero, por favor acuda al más cercano\n";
         }
-        return "Dinero: total " +getDinero()+ " euros \n"+
+        return "Dinero: total " +_dineroasacar.getDinero()+ " euros \n"+
                 "Billetes de " +bill500.getValor()+" : "  +getBilletes500()+"\n"+
                 "Billetes de "+bill200.getValor()+" : " +getBilletes200()+"\n"+
                 "Billetes de "+bill100.getValor()+" : " +getBilletes100()+"\n"+
